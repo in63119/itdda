3800742b2ad11e7955c4f2ff7f069c1265ea3b1e
심종훈 <sayhello.shim@gmail.com>
2021-02-21 23:56:47 +0900

    [SERVER] [FIX] https 배포관련 수정

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
+var morgan_1 = __importDefault(require("morgan"));
 var app = express_1.default();
 app.use(cors_1.default());
+app.use(morgan_1.default('dev'));
+// using morgan in TS
 app.use('/', function (req, res) {
-    res.send('welcome to the reach world!! hello!!');
+    res.send('welcome to datda world!! hello!!');
 });
 app.listen(5000, function () {
     console.log('server on 5000');
--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(cors());
 
+app.use(morgan('dev'));
+// using morgan in TS
+
 app.use('/', (req, res) => {
-  res.send('welcome to the reach world!! hello!!');
+  res.send('welcome to datda world!! hello!!');
 });
 
 app.listen(5000, () => {
