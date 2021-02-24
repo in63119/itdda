bd6f4477d06381986b9d550afbe0a74e75610709
심종훈 <sayhello.shim@gmail.com>
2021-02-24 15:08:37 +0900

    [SERVER] [FIX] typescript to javascript

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(cors());
 
 app.use(morgan('dev'));
-// using morgan in TS
 
 app.use('/', (req, res) => {
   res.send('welcome to datda world!! hello!!');
