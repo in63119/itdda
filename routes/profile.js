c5bbda9253b96b3cf86ee9b9cf23fdb26e894870
심종훈 <sayhello.shim@gmail.com>
2021-03-10 20:45:06 +0900

    [SERVER] [ADD] profile 관련 API 추가

--- routes/profile.js
+++ routes/profile.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const profileController = require('../controllers/profile');
 
 router.post('/', profileController.index);
+router.post('/searchinstitution', profileController.searchInstitution);
+router.post('/parentregister', profileController.parentRegister);
 
 module.exports = router;
