4e9be1f1786f9e6778a821511e0d2ee6545fb5f5
김지운 <expect.ta@gmail.com>
2021-07-01 16:24:30 +0900

    [CLIENT] FIX : CSS 수정

--- client/src/components/ClassManage.tsx
+++ client/src/components/ClassManage.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { css } from "styled-components";
 import { requestManageClass } from "../common/axios";
 import { useHistory } from "react-router-dom";
 
@@ -20,19 +20,13 @@ function ClassManage({
   setClassCheck,
 }: Props) {
   //클래스 추가 창
-
-  const history = useHistory();
   const [classInputs, setClassInputs] = useState({ className: "" });
-
-  //모달창 열기
-  const onModalClass = (value: any) => {
-    setModalVisible(true);
-    setModalMessage(value);
-  };
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
-    setClassInputs({ ...classInputs, className: value });
+    if (classInputs.className !== "") {
+      setClassInputs({ ...classInputs, className: value });
+    }
   };
 
   const manageClass = async (className: string, option: string) => {
@@ -47,59 +41,53 @@ function ClassManage({
       alert("요청이 되지 않았습니다");
     }
   };
-
   return isClass ? (
-    <div>
-      {classInfo.map((classs: any) => (
-        <div className="classArea">
-          <input
-            type="radio"
-            onChange={(e) => {
-              onChange(e);
-            }}
-            value={classs.className}
-            name="classList"
-          />
-          <span className="classList">{classs.className}</span>
-        </div>
-      ))}
+    <Wrapper>
       <ButtonArea>
-        <UnclickedButton
-          onClick={() => {
-            onModalClass(
-              <div>
-                <div>
-                  <span>반이름 : </span>
-                  <input
-                    type="text"
-                    placeholder="반 이름을 입력해주세요"
-                    onChange={(e) => {
-                      onChange(e);
-                    }}
-                  />
-                </div>
-              </div>,
-            );
-          }}
-        >
-          추가할 반 이름
-        </UnclickedButton>
-        <Button
+        <ClassInput
+          placeholder={"추가할 반을 입력해주세요"}
+          onChange={(e) => onChange(e)}
+        ></ClassInput>
+        <AddButton
           onClick={() => {
             manageClass(classInputs.className, "add");
           }}
         >
           반 추가
-        </Button>
-        <Button
-          onClick={() => {
-            manageClass(classInputs.className, "delete");
-          }}
-        >
-          삭제
-        </Button>
+        </AddButton>
       </ButtonArea>
-    </div>
+      <Container>
+        <ListSubtitle>
+          <span>선택</span>
+          <span>반</span>
+        </ListSubtitle>
+        <ListDiv>
+          {classInfo.map((classs: any) => (
+            <ClassWrapper className="classArea">
+              <input
+                type="radio"
+                onChange={(e) => {
+                  onChange(e);
+                }}
+                value={classs.className}
+                name="classList"
+              />
+              <span className="classList">{classs.className}</span>
+              <span>
+                <Button
+                  isActive={classs.className === classInputs.className}
+                  onClick={() => {
+                    manageClass(classInputs.className, "delete");
+                  }}
+                >
+                  삭제
+                </Button>
+              </span>
+            </ClassWrapper>
+          ))}
+        </ListDiv>
+      </Container>
+    </Wrapper>
   ) : (
     <div></div>
   );
@@ -107,16 +95,46 @@ function ClassManage({
 
 export default ClassManage;
 
-const Button = styled.button`
+const AddButton = styled.button`
   ${({ theme }) => theme.common.defaultButton}
-  margin : 0 2% 0 2%;
 `;
-
-const UnclickedButton = styled.button`
+const ListDiv = styled.div`
+  height: 80%;
+`;
+const ClassInput = styled.input`
+  ${({ theme }) => theme.common.defaultInput}
+`;
+const ClassWrapper = styled.div`
+  ${({ theme }) => theme.common.itemInListDiv}
+  grid-template-columns : repeat(3, 1fr);
+  place-items: center;
+  padding: 1%;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+`;
+const ListSubtitle = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  place-items: center;
+  font-size: 1.2rem;
+  border-bottom: 1px solid lightgray;
+  padding: 1%;
+`;
+const Container = styled.div`
+  ${({ theme }) => theme.common.listDiv}
+  height : 500px;
+`;
+const Button = styled.button<any>`
   ${({ theme }) => theme.common.unclickedButtonStyle}
-  margin : 0 2% 0 2%;
+  ${(props) =>
+    props.isActive &&
+    css`
+      ${({ theme }) => theme.common.defaultButton}
+    `}
 `;
-
 const ButtonArea = styled.div`
   margin-top: 2%;
+  text-align-last: end;
+  padding: 2%;
 `;
