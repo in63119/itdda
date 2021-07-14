f9bf1a22ed1d344182a279178ceff3f74a4e974b
김지운 <expect.ta@gmail.com>
2021-07-14 22:24:50 +0900

    [client/delet] delete unnecessary code.

--- client/src/components/Contents.tsx
+++ client/src/components/Contents.tsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import styled from "styled-components";
 import {
   AlbumCarousel,
   Timetable,
-  Carousel,
   MiniNotice,
   MIniIndiNotice,
   MainMenu,
