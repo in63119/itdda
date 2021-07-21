ab524b7c75c04ac10f60707b5d37254f6c6056b2
김지운 <expect.ta@gmail.com>
2021-07-21 20:58:52 +0900

    [server/ADD] ADD swagger api code

---
+++ server/modules/swagger/swagger.js
@@ -0,0 +1,30 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const options = {
+	swaggerDefinition: {
+		info: {
+			//스웨거 문서 작성될 내용들.
+			title: 'datda API',
+			version: '1.0.0',
+			description: 'datda API 문서',
+		},
+		//TODO : 배포링크로 전환 필요함.
+		host: 'localhost:5000',
+		basePath: '/',
+		contact: {
+			email: "expect.ta@gmail.com"
+		},
+		//TODO : api_key 등록 후 사용필요
+		// securityDefinitions: {
+		// 	api_key: {
+		// 		type: 'apiKey',
+		// 		in: 'header',
+		// 		name: 'api_key',
+		// 	}
+		// }
+	},
+	//1. routes 폴더 아래 .js파일로 정의, 2. swagger폴더 아래 swagger 설정
+	apis: ['./routes/*.js', './swagger/*']
+};
+const specs = swaggerJsdoc(options);
+module.exports = { swaggerUi, specs };
