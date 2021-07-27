68f8937d8b2b7609eba5147fe424863216ea9ae4
김지운 <expect.ta@gmail.com>
2021-07-27 20:28:53 +0900

    [server/ADD] add api

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -5,6 +5,50 @@ const profileController = require('../controllers/profile');
 /**
  * @swagger
  * /profile:
+ *   get:
+ *     summary: 
+ *       - 프로필 
+ *     tags: [Institution]
+ *     parameters : 
+ *       - in: header
+ *         name: authorization
+ *         required : true
+ *         description: access Token 필수입니다.
+ *         schema:
+ *           type: string
+ *           properties:
+ *             authorization:
+ *               type: string
+ *       - in: body
+ *         name: profileImage
+ *         required : true
+ *         description: 프로필 사진 변경시 body에 이미지를 제공해주세요
+ *         schema:
+ *           type: object
+ *           required:
+ *             - image 
+ *           properties:
+ *             profileImage:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: successfully response
+ *         schema:
+ *           type: object
+ *           properties:
+ *             profileImg:
+ *               type: string
+ *             name:
+ *               type : string
+ *             email:
+ *               type : string
+ *             mobile:
+ *               type : string
+ */
+router.post('/', profileController.index);
+/**
+ * @swagger
+ * /profile:
  *   put:
  *     summary: 
  *       - 프로필 사진 수정 
@@ -32,35 +76,20 @@ const profileController = require('../controllers/profile');
  *               type: string
  *     responses:
  *       200:
- *         description: successfully login
+ *         description: successfully response
  *         schema:
  *           type: object
  *           properties:
- *             accessToken:
+ *             profileImg:
  *               type: string
- *             permission:
+ *             name:
  *               type : string
- *             message:
+ *             email:
  *               type : string
- *               description : datda login succeeded 
- *       201:
- *         description: Created
- *         schema:
- *           type: object
- *           properties:
- *             message:
- *               type: string
- *               description : email does not exist
- *       202:
- *         description: Accepted
- *         schema:
- *           type: object
- *           properties:
- *             message:
- *               type: string
- *               description : wrong password
+ *             mobile:
+ *               type : string
  */
-router.post('/', profileController.index);
+router.put('/', profileController.modifyProfile);
 router.post('/searchinstitution', profileController.searchInstitution);
 router.post('/parentregister', profileController.parentRegister);
 
