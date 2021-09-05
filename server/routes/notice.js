ca1b543c1c2266f141246d1036cf937e4affabd9
김지운 <expect.ta@gmail.com>
2021-09-05 22:59:56 +0900

    notice API swagger 적용

--- server/routes/notice.js
+++ server/routes/notice.js
@@ -6,7 +6,7 @@ const noticeController = require('../controllers/notice');
  * @swagger
  * tags:
  *   name: Notice
- *   description: 공지사항 작성, 수정, 삭제
+ *   description: 공지사항 조회, 작성, 수정, 삭제
  * definitions:
  *   Notice:
  *     type: object
@@ -18,29 +18,34 @@ const noticeController = require('../controllers/notice');
  *       noticeId: 
  *         type: number
  *         description: 공지사항 id
+ *       category: 
+ *         type: string
+ *         description: notice or evnet
+ *       writer: 
+ *         type: string
+ *         description: 작성자
  *       title: 
  *         type: string
  *         description: 공지사항 제목
+ *       content: 
+ *         type: string
+ *         description: 내용
+ *       institutionId: 
+ *         type: number
+ *         description: 기관 id 
  *       createdAt: 
  *         type: string
  *         description: 생성일
  */
 
-router.post('/', noticeController.index);
 /**
  * @swagger
- * /notice?id={id}:
- *   delete:
+ * /notice:
+ *   post:
  *     summary:
- *       - 공지사항 삭제 
+ *       - 공지사항 조회
  *     tags: [Notice]
  *     parameters : 
- *       - in: query
- *         name: id 
- *         required : true
- *         description: 공지사항 삭제 시 noticeId 필수입니다.
- *         schema:
- *           type: number
  *       - in: header
  *         name: authorization
  *         required : true
@@ -50,6 +55,65 @@ router.post('/', noticeController.index);
  *           properties:
  *             authorization:
  *               type: string
+ *       - in: body
+ *         name: 
+ *         required: true
+ *         description: <childId 부모의 경우 필수> <title 글 업로드시 필수> <content  글 업로드시 필수> <category  글 업로드시 필수>
+ *         schema:
+ *           type: object
+ *           properties:
+ *             childId:
+ *               type: number
+ *             category:
+ *               type: string
+ *             title:
+ *               type: string
+ *             content:
+ *               type: string 
+ *     responses:
+ *       200:
+ *         description: successfully updated
+ *         schema:
+ *           type: object
+ *           properties:
+ *             ElEvent:
+ *               type: array
+ *               description : 행사
+ *               items:
+ *                 $ref: '#/definitions/Notice'
+ *             ElNotice:
+ *               type: array
+ *               description : 행사
+ *               items:
+ *                 $ref: '#/definitions/Notice'
+ *             noticeInfo:
+ *               type: array
+ *               description : 행사
+ *               items:
+ *                 $ref: '#/definitions/Notice'
+ */
+router.post('/', noticeController.index);
+/**
+ * @swagger
+ * /notice?id={id}:
+ *   delete:
+ *     summary:
+ *       - 공지사항 삭제 
+ *     tags: [Notice]
+ *     parameters : 
+ *       - in: body
+ *         name: updateNotice
+ *         required : true
+ *         description: body 객체에서 category는 필수적으로 존재 해야합니다. 수정이 필요한 요소가 있다면 해당 요소만 {key:avlue}로 보내주시면 됩니다.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             category:
+ *               type: string
+ *             title:
+ *               type: string
+ *             content:
+ *               type: string 
  *     responses:
  *       200:
  *         description: successfully deleted
